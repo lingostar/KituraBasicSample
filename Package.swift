67ce68b6819eca04fc900fc88e2e0efe49deee22
Seongkwan Yoon <admin@lingostar.co.kr>
2016-11-22 11:33:06 +0900

    Kitura Version match

--- Package.swift
+++ Package.swift
@@ -1,8 +1,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "KituraBasicSample",
+    name: "SuperTodoList",
+    targets:[
+        Target(name:"Server", dependencies:[.Target(name:"SuperTodoList")]),
+        Target(name:"SuperTodoList")
+    ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0)
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 0)
     ]
+
 )
---
+++ Sources/Server/main.swift
@@ -0,0 +1,11 @@
+import Kitura
+import HeliumLogger
+
+HeliumLogger.use()
+
+let router = Router()
+
+
+Kitura.addHTTPServer(onPort: 8090, with: router)
+Kitura.run()
+
